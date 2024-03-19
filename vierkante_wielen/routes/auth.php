33359f854681c66b7c1765e2f60135c3ea6488ba
Chris <166389@student.horizoncollege.nl>
2024-03-19 11:06:49 +0100

    laravel breeze geinstalleerd

--- vierkante_wielen/routes/auth.php
+++ vierkante_wielen/routes/auth.php
@@ -1,115 +1,31 @@
 <?php
 
-return [
+use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Defaults
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default authentication "guard" and password
-    | reset options for your application. You may change these defaults
-    | as required, but they're a perfect start for most applications.
-    |
-    */
+Route::middleware('guest')->group(function () {
+    Volt::route('register', 'pages.auth.register')
+        ->name('register');
 
-    'defaults' => [
-        'guard' => 'web',
-        'passwords' => 'users',
-    ],
+    Volt::route('login', 'pages.auth.login')
+        ->name('login');
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Guards
-    |--------------------------------------------------------------------------
-    |
-    | Next, you may define every authentication guard for your application.
-    | Of course, a great default configuration has been defined for you
-    | here which uses session storage and the Eloquent user provider.
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | Supported: "session"
-    |
-    */
+    Volt::route('forgot-password', 'pages.auth.forgot-password')
+        ->name('password.request');
 
-    'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-    ],
+    Volt::route('reset-password/{token}', 'pages.auth.reset-password')
+        ->name('password.reset');
+});
 
-    /*
-    |--------------------------------------------------------------------------
-    | User Providers
-    |--------------------------------------------------------------------------
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | If you have multiple user tables or models you may configure multiple
-    | sources which represent each model / table. These sources may then
-    | be assigned to any extra authentication guards you have defined.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
+Route::middleware('auth')->group(function () {
+    Volt::route('verify-email', 'pages.auth.verify-email')
+        ->name('verification.notice');
 
-    'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
-        ],
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Resetting Passwords
-    |--------------------------------------------------------------------------
-    |
-    | You may specify multiple password reset configurations if you have more
-    | than one user table or model in the application and you want to have
-    | separate password reset settings based on the specific user types.
-    |
-    | The expiry time is the number of minutes that each reset token will be
-    | considered valid. This security feature keeps tokens short-lived so
-    | they have less time to be guessed. You may change this as needed.
-    |
-    | The throttle setting is the number of seconds a user must wait before
-    | generating more password reset tokens. This prevents the user from
-    | quickly generating a very large amount of password reset tokens.
-    |
-    */
-
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => 'password_reset_tokens',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Confirmation Timeout
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define the amount of seconds before a password confirmation
-    | times out and the user is prompted to re-enter their password via the
-    | confirmation screen. By default, the timeout lasts for three hours.
-    |
-    */
-
-    'password_timeout' => 10800,
-
-];
+    Volt::route('confirm-password', 'pages.auth.confirm-password')
+        ->name('password.confirm');
+});
--- vierkante_wielen/routes/web.php
+++ vierkante_wielen/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormulierController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,24 +13,6 @@ use App\Http\Controllers\FormulierController;
 |
 */
 
-Route::get('autorijles-bij-ons', function () {
-    return view('autorijlesbijons');
-});
-
-Route::get('tarieven', function () {
-    return view('tarieven');
-});
-
-Route::get('over-ons', function () {
-    return view('over-ons');
-});
-
-Route::get('contact', function () {
-    return view('contact');
-});
-
-// Custom
-
 Route::view('/', 'index');
 
 Route::view('dashboard', 'dashboard')
@@ -43,8 +24,3 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 require __DIR__.'/auth.php';
-
-
-Route::get('/formulier', [FormulierController::class, 'createFormulier']);
-Route::post('/opslaan', [FormulierController::class, 'opslaanGegevens'])->name('opslaan');
-Route::get('/gegevens', [FormulierController::class, 'gegevensTonen']);
