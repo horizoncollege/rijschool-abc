7c8b66eeb889e514be6eeac22eda17ebab4026c7
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2024-04-12 11:03:30 +0200

    agenda doet het!!!!!

--- vierkante-wielen/app/Http/Controllers/DashboardController.php
+++ vierkante-wielen/app/Http/Controllers/DashboardController.php
@@ -15,7 +15,45 @@ class DashboardController extends Controller
     public function index()
     {
         // Haal de evenementen op uit de database
-        $events = Booking::all();
+
+        $events = array();
+        $bookings = Booking::all();
+
+        // hier moet pulldown komen met schakel,automaat en handicapt.
+        foreach ($bookings as $booking) {
+            $color = null;
+            if($booking->title == 'Schakel'){
+                $color =  '#924ACE';
+            }
+            if($booking->title == 'Automaat'){
+                $color =  '#68B01A';
+            }
+            if($booking->title == 'Handicapt'){
+                $color =  'red';
+            }
+            if($booking->title == 'schakel'){
+                $color =  '#924ACE';
+            }
+            if($booking->title == 'automaat'){
+                $color =  '#68B01A';
+            }
+            if($booking->title == 'handicapt'){
+                $color =  'red';
+            }
+
+            $events[] = [
+                'id' => $booking->id,
+                'title' => $booking->title,
+                'start' => $booking->start_date,
+                'end' => $booking->end_date,
+                'color' => $color,
+                // 'color' => 'black',
+                'textColor' => '#CBD5D4',
+                'border' => 'green',
+            ];
+        }
+
+
 
         // Stuur de evenementen naar de view
         return view('dashboard', compact('events'));
