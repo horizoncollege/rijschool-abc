d2474e296d160fee6f47513549a8a0dfd618b1ac
Chris <166389@student.horizoncollege.nl>
2024-04-12 08:45:16 +0200

    Login/aanmelden pagina's wijzigingen
    - Het logo van Vierkante Wielen is toegevoegd boven het login/aanmeld formulier.
    - De witte balken zijn nu uit de formulieren verwijderd.

--- vierkante-wielen/resources/views/layouts/guest.blade.php
+++ vierkante-wielen/resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="font-sans text-gray-900 antialiased bg-gray-100">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
