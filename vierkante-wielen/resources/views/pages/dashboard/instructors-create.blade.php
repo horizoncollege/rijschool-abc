bb643867848e5034a3560aacdc2be7a52b043436
Chris <166389@student.horizoncollege.nl>
2024-04-17 13:33:48 +0200

    Instructeur toevoegen formulier
    De styling van het formulier om een instructeur toe te voegen ziet er nu hetzelfde uit als de andere formulieren op de website.

--- vierkante-wielen/resources/views/pages/dashboard/instructors-create.blade.php
+++ vierkante-wielen/resources/views/pages/dashboard/instructors-create.blade.php
@@ -1,30 +1,38 @@
-    <!-- resources/views/instructors/create.blade.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Create Instructor</title>
-</head>
-<body>
-    <h1>Create New Instructor</h1>
-    <form method="POST" action="{{ route('instructors.store') }}">
-        @csrf
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name"><br>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-        <laber for="adres">Adres:</laber><br>
-        <input type="text" id="adres" name="adres"><br>
+    <div class="content">
+        <div class="table-text">
+            <div class="table-grey-contact">
+                <h2>Instructeur toevoegen</h2>
+                <div class="contact-form-seperate-left">
+                    <form method="POST" action="{{ route('instructors.store') }}">
+                        @csrf
+                        <label for="name">Naam:</label>
+                        <input type="text" id="name" name="name"><br><br>
 
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
+                        <label for="adres">Adres:</label>
+                        <input type="text" id="adres" name="adres"><br><br>
 
-        <label for="phone">Phone:</label><br>
-        <input type="text" id="phone" name="phone"><br>
+                        <label for="email">E-mail:</label>
+                        <input type="email" id="email" name="email"><br><br>
 
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br>
+                        <label for="phone">Telefoonnummer:</label>
+                        <input type="text" id="phone" name="phone"><br><br>
 
-        <button type="submit">Create</button>
-    </form>
-</body>
-</html>
+                        <label for="password">Wachtwoord:</label>
+                        <input type="password" id="password" name="password"><br><br>
 
+                        <div class="menu-buttons-404">
+                            <button class="yellow-button">Aanmaken</button></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
