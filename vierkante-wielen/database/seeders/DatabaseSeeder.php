ea96824d577b84e7ce7fc6da050c8b79957e23ff
Chris <166389@student.horizoncollege.nl>
2024-05-17 09:56:20 +0200

    Database seeder
    De seeder geeft nu informatie mee, zoals: telefoonnummer, geboortedatum, geslacht, adres, postcode en woonplaats.

--- vierkante-wielen/database/seeders/DatabaseSeeder.php
+++ vierkante-wielen/database/seeders/DatabaseSeeder.php
@@ -82,10 +82,20 @@ class DatabaseSeeder extends Seeder
             $roleStudent->givePermissionTo($permission);
         }
 
+        // Create a user with the 'Admin' role
+        $admins = [
+            ['name' => 'Rijschoolhouder', 'email' => 'rijschoolhouder@gmail.com', 'phone' => '0612345678', 'date_of_birth' => '3000-05-09', 'gender' => 'Rijschool', 'zip_code' => '1234 AB', 'adres' => 'Westerweg 26', 'city' => 'Limmen', 'password' => Hash::make('12345678')],
+        ];
+
+        foreach ($admins as $admin) {
+            $user = User::create($admin);
+            $user->assignRole($roleAdmin);
+        }
+
         // Create users with the 'Rijinstructeur' role
         $instructors = [
-            ['name' => 'Rijinstructeur 1', 'email' => 'rijinstructeur1@gmail.com', 'password' => Hash::make('12345678')],
-            ['name' => 'Rijinstructeur 2', 'email' => 'rijinstructeur2@gmail.com', 'password' => Hash::make('12345678')],
+            ['name' => 'Rijinstructeur 1', 'email' => 'rijinstructeur1@gmail.com', 'phone' => '0612345678', 'date_of_birth' => '2001-05-09', 'gender' => 'Man', 'zip_code' => '1234 AB', 'adres' => 'Westerweg 24', 'city' => 'Limmen', 'password' => Hash::make('12345678')],
+            ['name' => 'Rijinstructeur 2', 'email' => 'rijinstructeur2@gmail.com', 'phone' => '0612345678', 'date_of_birth' => '2000-05-08', 'gender' => 'Vrouw', 'zip_code' => '1234 AB', 'adres' => 'Lotharingenweg 6', 'city' => 'Alkmaar', 'password' => Hash::make('12345678')],
         ];
 
         foreach ($instructors as $instructor) {
@@ -95,8 +105,8 @@ class DatabaseSeeder extends Seeder
 
         // Create users with the 'Leerling' role
         $students = [
-            ['name' => 'Leerling1', 'email' => 'leerling1@gmail.com', 'password' => Hash::make('12345678')],
-            ['name' => 'Leerling2', 'email' => 'leerling2@gmail.com', 'password' => Hash::make('12345678')],
+            ['name' => 'Leerling1', 'email' => 'leerling1@gmail.com', 'phone' => '0612345678', 'date_of_birth' => '2006-05-09', 'gender' => 'Man', 'zip_code' => '1234 AB', 'adres' => 'Westerweg 26', 'city' => 'Limmen', 'password' => Hash::make('12345678')],
+            ['name' => 'Leerling2', 'email' => 'leerling2@gmail.com', 'phone' => '0612345678', 'date_of_birth' => '2005-05-09', 'gender' => 'Vrouw', 'zip_code' => '1234 AB', 'adres' => 'Westerweg 36', 'city' => 'Limmen', 'password' => Hash::make('12345678')],
         ];
 
         foreach ($students as $student) {
